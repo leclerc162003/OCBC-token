76d1781f24a95c1ab0aa2c3fbad8e7bba904c6cf
leclerc162003 <S10203330@connect.np.edu.sg>
2022-01-10 01:13:45 +0800

    added new home page

---
+++ app/src/main/java/sg/edu/np/onetokenocbc/newHomepage.java
@@ -0,0 +1,14 @@
+package sg.edu.np.onetokenocbc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newHomepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_homepage);
+    }
+}
