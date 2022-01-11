5be4079cc275907d4960af992c3aa8a703a43b72
leclerc162003 <S10203330@connect.np.edu.sg>
2022-01-11 21:03:45 +0800

    improved UI and fixed some constraints for new homepage

--- app/src/main/java/sg/edu/np/onetokenocbc/newHomepage.java
+++ app/src/main/java/sg/edu/np/onetokenocbc/newHomepage.java
@@ -1,14 +1,28 @@
 package sg.edu.np.onetokenocbc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class newHomepage extends AppCompatActivity {
 
+    public TextView test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_homepage);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_new_homepage);
+
     }
 }
