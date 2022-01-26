251761d708419a6664d3f33f544b6ea2fd0df936
Yi Fei Darius Kong <s10207996@connect.np.edu.sg>
2022-01-26 14:18:23 +0800

    implemented SMS function. Not tested

--- app/src/main/java/sg/edu/np/onetokenocbc/createJointMainHolder.java
+++ app/src/main/java/sg/edu/np/onetokenocbc/createJointMainHolder.java
@@ -5,15 +5,22 @@ import android.os.Bundle;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import com.twilio.twiml.MessagingResponse;
+import com.twilio.twiml.messaging.Body;
+import static spark.Spark.*;
+
+
 
 public class createJointMainHolder extends AppCompatActivity {
+    public static final String ACCOUNT_SID = System.getenv("AC82b9e5b1bbf2e0a9a3c6b8f851e65756");
+    public static final String AUTH_TOKEN = System.getenv("c47afab4512f541f06bb91e7f72cb252");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_joint_main_holder);
 
-
+        sendSms("97238972");
 
         //get data of main account holder from database
 
@@ -21,7 +28,35 @@ public class createJointMainHolder extends AppCompatActivity {
     }
     
     public static void sendSms(String number){
-        //sid: AC82b9e5b1bbf2e0a9a3c6b8f851e65756
-        // token: c47afab4512f541f06bb91e7f72cb252
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+16067662293"),
+                new com.twilio.type.PhoneNumber(number),
+                "Test message")
+                .create();
+
+        System.out.println(message.getSid());
     }
+
+    // to respond to an incomming text
+/*    public static void smsBuilder(String[] args) {
+        get("/", (req, res) -> "Hello Web");
+
+        post("/sms", (req, res) -> {
+            res.type("application/xml");
+            Body body = new Body
+                    .Builder("The Robots are coming! Head for the hills!")
+                    .build();
+            Message sms = new Message
+                    .Builder()
+                    .body(body)
+                    .build();
+            MessagingResponse twiml = new MessagingResponse
+                    .Builder()
+                    .message(sms)
+                    .build();
+            return twiml.toXml();
+        });
+    }*/
 }
