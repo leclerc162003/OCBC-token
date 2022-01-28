86dc18a3db46227c43557b9ba12a498412fc5ebf
Jun Hong <s10203330@connect.np.edu.sg>
2022-01-29 03:32:09 +0800

    added files for updateMainHolder details and jointHolderoption for joinholder to choose when creating account

---
+++ app/src/main/java/sg/edu/np/onetokenocbc/jointHolderoption.java
@@ -0,0 +1,15 @@
+package sg.edu.np.onetokenocbc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class jointHolderoption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joint_holderoption);
+        //chose between new or existing ocbc user
+    }
+}
---
+++ app/src/main/java/sg/edu/np/onetokenocbc/updateMainHolder.java
@@ -0,0 +1,16 @@
+package sg.edu.np.onetokenocbc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class updateMainHolder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_main_holder);
+
+        //when creating a new joint account main holder check his details and directs it to choose existing user or new user to scan IC
+    }
+}
